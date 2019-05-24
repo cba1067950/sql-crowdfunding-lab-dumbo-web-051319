8bb84770a61ff545ac012ecc710ac0b5597bc209
Joe Jamsky <cba1067950@yahoo.com>
2019-05-24 12:58:22 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -13,7 +13,10 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
   # GROUP BY title HAVING SUM(amount)
-  "SELECT projects.title, Sum(pledges.amount) FROM projects INNER JOIN pledges ON project_id = projects.id GROUP BY projects.title;"
+  "SELECT projects.title, Sum(pledges.amount) 
+  FROM projects 
+  INNER JOIN pledges ON project_id = projects.id 
+  GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
